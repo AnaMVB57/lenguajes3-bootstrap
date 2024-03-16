9be1b0d8950f33ec783c698e59ccc5c5c4fdd1b9
AnaMVB57 <108105538+AnaMVB57@users.noreply.github.com>
2024-03-16 11:47:20 -0500

    Add files via upload

---
+++ ejemplo04/ejemplo04/src/main/java/com/anavillanueva/ejemplo04/Formularios.java
@@ -0,0 +1,15 @@
+package com.anavillanueva.ejemplo04;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Formularios {
+
+	@GetMapping(value = "/principal")
+	public String Calculo() {
+		
+		return "ingreso";
+	}
+	
+}
